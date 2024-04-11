12875e16f79d01421e87b925a0f0927ecf29e200
Yeison Echavaria <yeisonlinares002@outlook.com>
2024-04-11 12:02:57 -0400

    Agregado nuevo archivo AppUI que almacena parte de la interfaz, tambien modificado el index de la carpeta App y useLocalStorage

--- curso-react-intro/src/App/index.js
+++ curso-react-intro/src/App/index.js
@@ -1,106 +1,14 @@
 import React from 'react';
-import { TodoCounter, LoadingView } from '../TodoCounter';
-import { TodoSearch } from '../TodoSearch';
-import { TodoList } from '../TodoList';
-import { TodoItem } from '../TodoItem/TodoItem';
-import { TodosLoading } from '../TodosLoading';
-import { TodosError } from '../TodosError';
-import { EmptyTodos } from '../EmptyTodos';
-import { CreateTodoButton } from '../CreateTodoButton';
-import { useLocalStorage } from './useLocalStorage';
-
-
-// localStorage.removeItem('TODOS_V1')
-// const defaultTodos = [
-//   { text: 'Cortar cebolla', completed: false },
-//   { text: 'Tomar el Curso de Intro a React.js', completed: false },
-//   { text: 'Llorar con la Llorona', completed: false },
-//   { text: 'LALALALALA', completed: false },
-//   { text: 'Usar estados derivados', completed: false },
-// ];
-
-// localStorage.setItem('TODOS_V1', JSON.stringify(defaultTodos))
+import { TodoProvider } from '../TodoContext';
+import { AppUI } from './AppUI';
 
 
 function App() {
 
-  const {
-    item: todos,
-    saveItem: saveTodos,
-    loading,
-    error,
-    isLoading,
-   } = useLocalStorage('TODOS_V1', [])
-
-  const [searchValue, setSearchValue] = React.useState('');
-
-  const completedTodos = todos.filter(
-    todo => !!todo.completed
-  ).length;
-  const totalTodos = todos.length;
-
-  const searchedTodos = todos.filter(
-    (todo) => {
-      const todoText = todo.text.toLowerCase();
-      const searchText = searchValue.toLowerCase();
-      return todoText.includes(searchText);
-    }
-  );
-
-  const completeTodo = (text) => {
-    const newTodos = [...todos]
-    const todoIndex = newTodos.findIndex(
-      (todo) => todo.text === text
-    )
-    newTodos[todoIndex].completed = true
-    saveTodos(newTodos)
-  }
-  const deleteTodo = (text) => {
-    const newTodos = [...todos]
-    const todoIndex = newTodos.findIndex(
-      (todo) => todo.text === text
-    )
-    newTodos.splice(todoIndex, 1) 
-    saveTodos(newTodos)
-  }
-
   return (
-    <>
-      {isLoading ? (<LoadingView />) : (
-      <TodoCounter
-        completed={completedTodos}
-        total={totalTodos}
-      /> )
-      }
-
-      <TodoSearch
-        searchValue={searchValue}
-        setSearchValue={setSearchValue}
-      />
-
-      <TodoList>
-        {loading &&
-        <>
-        <TodosLoading />
-        <TodosLoading />
-        <TodosLoading />
-        </>}
-        {error && <TodosError />}
-        {(!loading && searchedTodos.length === 0) && <EmptyTodos />}
-      
-        {searchedTodos.map(todo => (
-          <TodoItem
-          key={todo.text}
-          text={todo.text}
-          completed={todo.completed}
-          onComplete={ () => completeTodo(todo.text)}
-          onDelete={() => deleteTodo(todo.text)}
-          />
-        ))}
-      </TodoList>
-      
-      <CreateTodoButton />
-    </>
+    <TodoProvider>
+      <AppUI />
+    </TodoProvider>
   );
 }
 
---
+++ curso-react-intro/src/TodoContext/index.js
@@ -0,0 +1,78 @@
+import React from "react";
+import { useLocalStorage } from "./useLocalStorage";
+
+const TodoContext = React.createContext()
+
+function TodoProvider({ children }) {
+
+    const {
+        item: todos,
+        saveItem: saveTodos,
+        loading,
+        error,
+        isLoading,
+    } = useLocalStorage('TODOS_V1', [])
+    
+    const [searchValue, setSearchValue] = React.useState('');
+
+    const completedTodos = todos.filter(
+        todo => !!todo.completed
+    ).length;
+      const totalTodos = todos.length;
+    
+      const searchedTodos = todos.filter(
+        (todo) => {
+          const todoText = todo.text.toLowerCase();
+          const searchText = searchValue.toLowerCase();
+          return todoText.includes(searchText);
+        }
+    );
+    
+    const completeTodo = (text) => {
+    const newTodos = [...todos]
+    const todoIndex = newTodos.findIndex(
+        (todo) => todo.text === text
+    )
+        newTodos[todoIndex].completed = true
+        saveTodos(newTodos)
+}
+    const deleteTodo = (text) => {
+    const newTodos = [...todos]
+    const todoIndex = newTodos.findIndex(
+        (todo) => todo.text === text
+    )
+    newTodos.splice(todoIndex, 1) 
+    saveTodos(newTodos)
+}
+
+    return (
+        <TodoContext.Provider value={{ 
+            isLoading,
+            loading,
+            error,
+            completedTodos,
+            totalTodos,
+            searchValue,
+            setSearchValue,
+            searchedTodos,
+            completeTodo,
+            deleteTodo,
+         }}>
+            {children}
+        </TodoContext.Provider>
+    )
+}
+
+export { TodoContext, TodoProvider }
+
+
+// localStorage.removeItem('TODOS_V1')
+// const defaultTodos = [
+//   { text: 'Cortar cebolla', completed: false },
+//   { text: 'Tomar el Curso de Intro a React.js', completed: false },
+//   { text: 'Llorar con la Llorona', completed: false },
+//   { text: 'LALALALALA', completed: false },
+//   { text: 'Usar estados derivados', completed: false },
+// ];
+
+// localStorage.setItem('TODOS_V1', JSON.stringify(defaultTodos))
